b713bce29cc0915fdb4467897b3b976f235e1a8f
martunis <47129938+martunismukhtar@users.noreply.github.com>
2025-07-26 10:49:34 +0700

    Update experienceData.js
    memperbaiki pengalaman kerja

--- src/Konstanta/experienceData.js
+++ src/Konstanta/experienceData.js
@@ -1,34 +1,41 @@
 const experiences=[
     {
-        year:"06/2024 - 12/2024",
-        company:"Dinas PUPR Kabupaten Aceh Barat",
-        link:"https://simatabaru.acehbesarkab.go.id/app/matras/",
+        year:"January 2024 – Present",
+        company:"Aceh Government",
+        link:"http://62.72.13.172:8383/",
         position:"Full Stack Developer",
         description:"Headed the development of a Geospatial Information System (GIS) for monitoring and analyzing land use, utilizing advanced mapping technologies and spatial data analysis tools. The system was designed to automate the process of land use assessment by integrating satellite imagery, geographic data, and real-time location data. Through this system, users were able to efficiently identify land use patterns, track changes over time, and make informed decisions related to urban planning, environmental management, and land resource allocation.",
-        techs:["React JS", "Tailwind CSS", "Laravel", "Openlayers", "Geoserver", "Postgresql"]
+        techs:["Laravel", "Openlayers", "Geoserver", "Postgresql"]
     }, {
-        year:"01/2024 - 06/2024",
+        year:"2023 – 2024",
         company:"InnovateGeo",
         link:"https://innovategeo.org/",
-        position:"Full Stack Developer - Part-time",
+        position:"Full Stack Developer",
         description:"Contributed to the development of a land survey application aimed at simplifying the process of data collection, analysis, and visualization related to land use and conditions. Key features developed include interactive survey forms, geotagging systems for real-time mapping of survey points, and the ability to upload photos and supporting documents. The application was designed to provide a user-friendly platform for land surveying and data analysis, making it easier for land owners and planners to gather and analyze land use information.",
         techs:["Laravel", "Openlayers", "Geoserver", "Postgresql"]
     }, {
-        year:"06/2022 - 06/2024",
+        year:"2022 - 2023",
         company:"AviationLearn",
         link:"https://www.aviationlearn.com/",
         position:"Full Stack Developer",
         description:"Participated in the development of a Learning Management System (LMS) and played a key role in the creation of the AviationLearn website. The project focused on building a comprehensive online learning platform tailored to the aviation industry, providing users with a range of interactive courses, certifications, and training resources. In addition to developing the core features of the LMS, such as user management, progress tracking, and course delivery systems, I contributed to designing the AviationLearn website, ensuring it was user-friendly, visually appealing, and fully responsive across various devices. The platform was optimized for a seamless learning experience, with features like course recommendations, discussion forums, and assessments, aimed at enhancing both instructor and learner engagement.",
         techs:["Laravel", "React JS", "Tailwind CSS"]
     }, {
-        year:"03/2021 - 03/2022",
+        year:"2021 - 2022",
+        company:"Dinas PUPR Aceh Barat",
+        link:"http://pupracehbarat.id/mapsitrab/",
+        position:"Full Stack Developer -part-time",
+        description:"Built full-stack web application for land availability checking.",
+        techs:["CodeIgniter", "React JS", "Tailwind CSS", "Openlayers", "Geoserver", "Postgresql"]
+    }, {
+        year:"2021 - 2022",
         company:"MoolahGo",
         link:"https://www.moolahgo.com/",
         position:"Full Stack Developer",
         description:"Collaborated closely with cross-functional teams to design and implement new features on the admin side, aimed at improving efficiency in data management. These enhancements included streamlined workflows, advanced filtering and search capabilities, automated data validation processes, and customizable dashboards to provide real-time insights. Focused on optimizing the user experience for administrators, ensuring that the tools were intuitive, scalable, and aligned with operational requirements. Additionally, contributed to testing and refining these features to ensure seamless integration with existing systems, ultimately reducing manual effort, minimizing errors, and enhancing overall productivity in data management operations.",
         techs:["CodeIgniter 4", "JQuery", "Bootstrap"]
     }, {
-        year:"03/2021 - 03/2022",
+        year:"2020 - 2021",
         company:"immap",
         link:"https://careers.immap.org/",
         position:"Full Stack Developer",
